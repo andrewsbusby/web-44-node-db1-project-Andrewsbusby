a9fd81c0cf47c9164664ef83ec890d19ec20c6f9
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-09 15:00:40 -0600

    removing lines for eslint

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/:id', md.checkAccountId, async (req, res, next) => {
+router.get('/:id', md.checkAccountId, async (req, res, next) => { //eslint-disable-line
   res.json(req.account)
 })
 
