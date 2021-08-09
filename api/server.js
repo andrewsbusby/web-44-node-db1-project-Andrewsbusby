e7e0f2aebe4c189fd8b618a09ee38c5888c3e4e0
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-09 13:36:01 -0600

    setting up router in server

--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const accoutnsRouter = require('./accounts/accounts-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('*', (req, res) => {
+    res.status(404).json({message: 'not found'})
+})
 
 module.exports = server;
